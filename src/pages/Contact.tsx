62aa7bcc9fcd31f543eef86062bb067e20b09f01
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2026-09-23 14:02:50 +0530

    db chronlise

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { db } from "@/firebase";
+import { db } from "../firebase";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { 
